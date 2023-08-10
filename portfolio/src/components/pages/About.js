e39a9255b04078890fea54084d071bc04a126e72
gcot970 <gcot970@gmail.com>
2023-08-10 14:45:30 -0500

    About page done. Started work on Contact

---
+++ portfolio/src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <div className="about-content">
+                <p>Hi there! My name is Gage Cotton, and I'm a passionate junior web developer with a knack for creating engaging online experiences. With a strong foundation in computer troubleshooting and running SMB systems spanning five years, I bring a wealth of technical knowledge to my development journey.</p>
+                <p>In the realm of web development, I've honed my skills in various technologies:</p>
+                <ul>
+                    <li>Node.js</li>
+                    <li>MySQL and NoSQL databases</li>
+                    <li>Express.js</li>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                </ul>
+                <p>My goal is to craft clean, efficient, and visually appealing web solutions that leave a positive impact on users. Thank you for taking an interest in my journey as a developer!</p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ portfolio/src/components/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Incomplete... gotta add the form and stuff
+
+function Contact() {
+    return (
+        <div>
+            <h2>Contact Me</h2>
+            <div className="contact-content">
+                <ul>
+                    <li>Email</li>
+                    <li>GitHub</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
